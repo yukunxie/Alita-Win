6c24655131f2684f920a1da835e4ab2b95c84bda
realxie <jinyun2007@126.com>
2021-03-08 00:57:23 +0800

    update

--- Alita/Sources/World/World.cpp
+++ Alita/Sources/World/World.cpp
@@ -32,7 +32,8 @@ World::World()
 	{
 		//const auto& models = GLTFLoader::LoadModelFromGLTF("Models/chinesedragon.gltf");
 		//const auto& models = GLTFLoader::LoadModelFromGLTF("Models/color_teapot_spheres.gltf");
-		const auto& models = GLTFLoader::LoadModelFromGLTF("Models/deer.gltf");
+		//const auto& models = GLTFLoader::LoadModelFromGLTF("Models/deer.gltf");
+		const auto& models = GLTFLoader::LoadModelFromGLTF("Models/armor/armor.gltf");
 		for (auto model : models)
 		{
 			AddEntity(model);
