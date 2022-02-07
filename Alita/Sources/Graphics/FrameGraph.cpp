6b15c8cb12f415797b8333e674842c8c8617f922
realxie <jinyun2007@126.com>
2022-02-08 00:41:32 +0800

    FrameGraph

---
+++ Alita/Sources/Graphics/FrameGraph.cpp
@@ -0,0 +1,18 @@
+//
+// Created by realxie on 2022-01-06.
+//
+
+#include "FrameGraph.h"
+#include "World/MeshComponent.h"
+#include "World/World.h"
+#include "World/Camera.h"
+#include "RenderObject.h"
+#include "Engine/Engine.h"
+#include "RenderScene.h"
+#include "Material.h"
+
+NS_RX_BEGIN
+
+
+
+NS_RX_END
---
+++ Alita/Sources/Graphics/FrameGraph.h
@@ -0,0 +1,21 @@
+//
+// Created by realxie on 2022-01-06.
+//
+
+#pragma once
+
+#include "Base/ObjectBase.h"
+#include "GFX/GFX.h"
+#include "Types/Types.h"
+#include "World/Camera.h"
+#include "Technique.h"
+#include "RenderTarget.h"
+#include "Texture.h"
+
+NS_RX_BEGIN
+
+
+
+NS_RX_END
+
+
